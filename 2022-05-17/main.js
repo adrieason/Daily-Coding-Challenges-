0de009c56b360a275d81e301124885554e0c1cd2
Adrianna Eason <adrianname@icloud.com>
2022-05-17 19:17:22 -0500

    Easy Codewars Day

---
+++ 2022-05-17/main.js
@@ -0,0 +1,11 @@
+//Complete the solution so that it reverses the string passed into it.
+function solution(str){
+  return str.split('').reverse().join('')
+}
+
+//The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+function sayHello(name) {
+  return 'Hello, ' + name
+}
+
