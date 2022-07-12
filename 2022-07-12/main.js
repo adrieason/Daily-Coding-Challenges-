503a282aa8ccb9ba89eb66f0fbeec2677d04a1ca
Adrianna Eason <adrianname@icloud.com>
2022-07-12 17:35:55 -0500

    CC for 07-12-2022

---
+++ 2022-07-12/main.js
@@ -0,0 +1,9 @@
+//debug the code! 
+
+function rainAmount(mm){
+  if (mm < 40) {
+       return "You need to give your plant " + (40-mm) + "mm of water"
+  }else if (mm>=40){
+       return "Your plant has had more than enough water for today!"
+  };
+}
