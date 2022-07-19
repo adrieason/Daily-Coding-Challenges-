890ba76f1289c098542fe2a954cde69a5faf1566
Adrianna Eason <adrianname@icloud.com>
2022-07-18 19:33:06 -0500

    07-14-2022

---
+++ 2022-07-14/main.js
@@ -0,0 +1,13 @@
+/*Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."*/
+
+var Ball = function(ballType) {
+	if (!ballType){
+		this.ballType = 'regular';
+	}
+	else {
+		this.ballType= 'super';
+	}
+
+}
