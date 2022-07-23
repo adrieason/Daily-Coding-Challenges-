4c069c5b11be1882ec7625a85367ab217f36e7b5
Adrianna Eason <adrianname@icloud.com>
2022-07-23 10:56:43 -0500

    07-23-2022

---
+++ 2022-07-23/main.js
@@ -0,0 +1,18 @@
+/*Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.*/
+
+function getChar(c) {
+  return String.fromCharCode(c)
+}
+
+/*Complete the method which accepts an array of integers, and returns one of the following:
+
+"yes, ascending" - if the numbers in the array are sorted in an ascending order
+"yes, descending" - if the numbers in the array are sorted in a descending order
+"no" - otherwise
+You can assume the array will always be valid, and there will always be one correct answer.*/
+
+function isSortedAndHow(arr) {
+  return arr.every((x,i)=>i==0||arr[i]>=arr[i-1])?'yes, ascending':
+         arr.every((x,i)=>i==0||arr[i]<=arr[i-1])?'yes, descending':'no'
+}
+
