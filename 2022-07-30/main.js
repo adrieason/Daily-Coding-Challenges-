9ec673437b0d4dfa9ab7b0626a827ae029e2569a
Adrianna Eason <adrianname@icloud.com>
2022-07-30 20:21:39 -0500

    07-30-2022

---
+++ 2022-07-30/main.js
@@ -0,0 +1,63 @@
+/*You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+
+Your function will accept three arguments:
+The first and second argument should be numbers.
+The third argument should represent a sign indicating the operation to perform on these two numbers.
+
+if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned*/
+
+function calculator(a,b,sign){
+  if (sign==="+"){
+    return a+b
+  }else if(sign === "-"){
+    return a-b
+  }else if (sign === "/"){
+    return a/b
+  }else if (sign==="*"){
+    return a*b
+  }else{
+    return "unknown value"
+  }
+}
+
+/*Introduction
+
+There is a war and nobody knows - the alphabet war!
+There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+
+Task
+
+Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+*/
+
+function alphabetWar(fight) {
+  let map = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 };
+  let result = fight.split('').reduce((a, b) => a + (map[b] || 0), 0);
+  return result ? (result < 0 ? "Left" : "Right") + " side wins!" : "Let's fight again!";
+}
+
+/*All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.*/
+
+function feast(beast, dish) {
+  return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+}
+
+/*Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+4 --> 0 (because 4 is already a one-digit number)*/
+
+function persistence(num) {
+  for (var i = 0; num > 9; i++) {
+    num = num.toString().split('').reduce((t, c) => c * t);
+  }
+  return i;
+}
