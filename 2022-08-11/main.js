37063810e59e9b553e9dfbe0df1c29d1b398b4e4
Adrianna Eason <adrianname@icloud.com>
2022-08-11 09:35:57 -0500

    CC for 08-11-2022

---
+++ 2022-08-11/main.js
@@ -0,0 +1,7 @@
+/*Create a function that takes a string and an integer (n).
+
+The function should return a string that repeats the input string n number of times.*/
+
+var repeatIt = function(str, n) {
+  return str.repeat(n)
+  }
