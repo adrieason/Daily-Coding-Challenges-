4a3026810ed35106920c8e11f6cf43c2e87d6e90
Adrianna Eason <adrianname@icloud.com>
2022-09-29 20:24:06 -0500

    2022-09-29

---
+++ 2022-09-29/main.js
@@ -0,0 +1,7 @@
+//Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+function shortcut(str) {
+  return str.split('').filter(function(e) {
+    return ['a', 'e', 'i', 'o', 'u'].indexOf(e) == -1 
+  }).join('')
+}
