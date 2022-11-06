782286b034ee031ddf68d05eb03792d2ea1f1e4c
Adrianna Eason <adrianname@icloud.com>
2022-11-06 12:10:19 -0600

    CC for 2022-11-06

--- 2022-11-04/main.js
+++ 2022-11-04/main.js
@@ -1,56 +1,24 @@
-/*There are no explanations. You have to create the code that gives the following results in Python, Ruby, and Haskell:
+/*If you've completed this kata already and want a bigger challenge, here's the 3D version
 
-one_two_three(0) == [0, 0]
-one_two_three(1) == [1, 1]
-one_two_three(2) == [2, 11]
-one_two_three(3) == [3, 111]
-one_two_three(19) == [991, 1111111111111111111]
-And it should give the following results in Javascript, Scala, D, Go, and Rust:
+Bob is bored during his physics lessons so he's built himself a toy box to help pass the time. The box is special because it has the ability to change gravity.
 
-oneTwoThree(0) == ['0', '0']
-oneTwoThree(1) == ['1', '1']
-oneTwoThree(3) == ['3', '111']
-oneTwoThree(19) == ['991', '1111111111111111111']
-In C, the results are to be assigned to seperate pointers.
-*/
+There are some columns of toy cubes in the box arranged in a line. The i-th column contains a_i cubes. At first, the gravity in the box is pulling the cubes downwards. When Bob switches the gravity, it begins to pull all the cubes to a certain side of the box, d, which can be either 'L' or 'R' (left or right). Below is an example of what a box of cubes might look like before and after switching gravity.
 
-function oneTwoThree(n) {
-  //create array 
-    let num = n.toString()
-    let array = []
-    if (n===0){
-      return ['0','0']
-    }else{
-      array.push('1'.repeat(n))
-      if(n<10){
-        array.unshift(num)
-      }if(n>9){
-        array.unshift(num[1].repeat(2)+num[0])
-      }if(n>99){
-      }
-    }  
-    return array
++---+                                       +---+
+|   |                                       |   |
++---+                                       +---+
++---++---+     +---+              +---++---++---+
+|   ||   |     |   |   -->        |   ||   ||   |
++---++---+     +---+              +---++---++---+
++---++---++---++---+         +---++---++---++---+
+|   ||   ||   ||   |         |   ||   ||   ||   |
++---++---++---++---+         +---++---++---++---+
+Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity.*/
+
+const flip=(d, a)=>{ 
+  if (d==='R'){
+    return a.sort((a,b)=>a-b)
+  }else{
+    return a.sort((a,b)=>b-a)
   }
-  
-  // 0 = [0,0]
-  // 1 = [1,1]
-  // 19 = [991,1111111111111]
-
-
-  //I didn't get the code correct. Here is the correct solution. 
-  function oneTwoThree(n) {
-    if (n==0) //if n is 0 then just return the answer is [0,0]
-      return ['0','0']
-    var s=''; //set empty strings 
-    var c=''
-    for (var i=0; i<n; ++i) //loops through for number 
-      s+='1'; //adds '1' to the string
-    while (n>9)//if it's less than 9 add '9' to c and subtract that 9 from n 
-    {
-      c+='9';
-      n-=9;
-    }
-    if (n!=0) //if n is not 0, add n to c
-      c+=n
-    return [c,s]; return //c,s as an array
 }
