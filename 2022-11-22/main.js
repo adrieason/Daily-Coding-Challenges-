1c26f1adfc93ee277a070d779aa0d0cb6340a8ef
Adrianna Eason <adrianname@icloud.com>
2022-11-22 21:12:35 -0600

    CC for 2022-11-22

---
+++ 2022-11-22/main.js
@@ -0,0 +1,52 @@
+/*The purpose of this kata is to work out just how many bottles of duty free whiskey you would have to buy such that the saving over the normal high street price would effectively cover the cost of your holiday.
+
+You will be given the high street price (normPrice), the duty free discount (discount) and the cost of the holiday.
+
+For example, if a bottle cost £10 normally and the discount in duty free was 10%, you would save £1 per bottle. If your holiday cost £500, the answer you should return would be 500.
+
+All inputs will be integers. Please return an integer. Round down.*/
+
+function dutyFree(normPrice, discount, hol){
+  let savings = normPrice*discount/100
+  return Math.floor(hol/savings)
+}
+
+/*Your task, is to create NxN multiplication table, of size provided in parameter.
+
+for example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]*/
+
+multiplicationTable = function(size) {
+  let arr = []
+  for (i=1;i<=size;i++){
+    let subArr = []
+  for (j=1;j<=size;j++){
+    subArr.push(i*j)
+  }
+   arr.push(subArr)
+  }
+  return arr
+}
+
+/*Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+For example, decode("h3 th2r2") would return "hi there".
+
+For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.*/ 
+
+const table = ['a', 'e', 'i', 'o', 'u']
+const encode = string => string.split('').map(x => table.indexOf(x) + 1 || x).join('')
+const decode = string => string.split('').map(x => table[Number(x) - 1] || x).join('')
