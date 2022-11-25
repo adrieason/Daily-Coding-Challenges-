f80001ea749523126602fe38a90c54fc31105ddc
Adrianna Eason <adrianname@icloud.com>
2022-11-24 19:34:44 -0600

    CC for 2022-11-24

---
+++ 2022-11-24/main.js
@@ -0,0 +1,30 @@
+/*ou're re-designing a blog and the blog's posts have the following format for showing the date and time a post was made:
+
+Weekday Month Day, time e.g., Friday May 2, 7pm
+
+You're running out of screen real estate, and on some pages you want to display a shorter format, Weekday Month Day that omits the time.
+
+Write a function, shortenToDate, that takes the Website date/time in its original string format, and returns the shortened format.
+
+Assume shortenToDate's input will always be a string, e.g. "Friday May 2, 7pm". Assume shortenToDate's output will be the shortened string, e.g., "Friday May 2".*/
+
+function shortenToDate(longDate) {
+  return longDate.split(',')[0]
+}
+
+/*Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+If n == 0 return an empty sequence []
+
+*/
+
+function first(arr, n) {
+  if (n==0){
+    return []
+  }
+  if (n>=1){
+    return arr.splice(0,n)
+  }else{
+    return [arr[0]]
+  }
+}
