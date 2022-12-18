74160c61f62fa425b5d9ad2c46a3ea6dddc00aa0
Adrianna Eason <adrianname@icloud.com>
2022-12-18 08:53:03 -0600

    CC for 2022-12-18

---
+++ 2022-12-17/main.js
@@ -0,0 +1,12 @@
+/*Given Two integers a , b , find The sum of them , BUT You are not allowed to use the operators + and -*/
+
+function add(x, y){
+ // let arr = ([x,y]).sort((a,b)=>a-b)
+ // let count = arr[0]
+ // for (i=0; i<arr[1]; i++){
+ //   count ++
+ // }
+ // return count
+  
+
+}
