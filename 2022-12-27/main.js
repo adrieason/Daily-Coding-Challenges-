e8ba5f8bd796d7e4a3b879ac1c61c8aff2b9bb97
Adrianna Eason <adrianname@icloud.com>
2022-12-28 20:51:00 -0600

    CC for 2022-12-28

---
+++ 2022-12-27/main.js
@@ -0,0 +1,38 @@
+/*This series of katas will introduce you to basics of doing geometry with computers.
+
+Point objects have x and y attributes (X and Y in C#) attributes.
+
+Write a function calculating distance between Point a and Point b.
+
+Tests round answers to 6 decimal places.*/
+
+function distanceBetweenPoints(a, b) {
+  return Math.sqrt(Math.pow((a.x-b.x),2)+Math.pow((a.y-b.y),2))
+}
+
+/*Sometimes, I want to quickly be able to convert miles per imperial gallon into kilometers per liter.
+
+Create an application that will display the number of kilometers per liter (output) based on the number of miles per imperial gallon (input).
+
+Make sure to round off the result to two decimal points.
+
+Some useful associations relevant to this kata: 1 Imperial Gallon = 4.54609188 litres 1 Mile = 1.609344 kilometres*/
+
+function converter (mpg) {
+  return Math.round(mpg*1.609344/4.54609188*100)/100
+}
+
+/*Task
+Implement a function which accepts 2 arguments: string and separator.
+
+The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.*/
+
+function splitAndMerge(string, separator) {
+  let arr = string.split(' ')
+  let newArr = []
+  for(i=0;i<arr.length;i++){
+    newArr.push(arr[i].split('').join(separator))
+  }
+  return newArr.join(' ')
+}
+
---
+++ 2022-12-28/main.js
@@ -0,0 +1,51 @@
+/*In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+
+The result should also be ordered from highest to lowest.
+
+Examples:
+
+[4, 10, 10, 9]  =>  [10, 9]
+[1, 1, 1]  =>  [1]
+[]  =>  []*/
+
+function twoHighest(arr) {
+  arr = [...new Set(arr)]
+  let array = arr.sort((a,b)=>a-b)
+  return arr.splice(arr.length-2,2).reverse()
+}
+
+/*Task:
+Mr. despair wants to jump off Dutch act, So he came to the top of a building.
+
+Scientific research shows that a man jumped from the top of the roof, when the floor more than 6, the person will often die in an instant; When the floor is less than or equal to 6, the person will not immediately die, he would scream. (without proof)
+
+Input: floor, The height of the building (floor)
+
+Output: a string, The voice of despair(When jumping Dutch act)
+
+Example:
+sc(2) should return "Aa~ Pa! Aa!"
+
+It means:
+
+Mr. despair jumped from the 2 floor, the voice is "Aa~"
+He fell on the ground, the voice is "Pa!"
+He did not die immediately, and the final voice was "Aa!"
+sc(6) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Pa! Aa!"
+
+sc(7) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!"
+
+sc(10) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!"
+
+if floor<=1, Mr. despair is safe, return ""*/
+
+function sc(floor){
+  if (floor<=1){
+    return ''
+  }
+  if (floor<=6){
+  return "Aa~ ".repeat(floor-1)+'Pa! Aa!'
+}else if (floor>6){
+    return "Aa~ ".repeat(floor-1)+'Pa!'
+}
+  }
