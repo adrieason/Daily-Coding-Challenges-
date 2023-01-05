2e4b7977b609780d4f92e6c5502b06df0280f570
Adrianna Eason <adrianname@icloud.com>
2023-01-05 17:27:09 -0600

    2023-01-05

--- 2023-01-05/main.js
+++ 2023-01-05/main.js
@@ -1,9 +1,25 @@
-/*DESCRIPTION:
-A cyclops number is a number in binary that is made up of all 1's, with one 0 in the exact middle. That means all cyclops numbers must have an odd number of digits for there to be an exact middle.
-A couple examples:
-101
-11111111011111111
-You must take an input, n, that will be in decimal format (base 10), then return True if that number wil be a cyclops number when converted to binary, or False if it won't.
-Assume n will be a positive integer.*/ 
+/*Given an unsorted array of integers, find the smallest number in the array, the largest number in the array, and the smallest number between the two array bounds that is not in the array.
 
-const cyclops =(n, b=n.toString(2).split('0'))=> b.length==2 && b[0]==b[1]
+For instance, given the array [-1, 4, 5, -23, 24], the smallest number is -23, the largest number is 24, and the smallest number between the array bounds is -22. You may assume the input is well-formed.
+
+You solution should return an array [smallest, minimumAbsent, largest]
+
+The smallest integer should be the integer from the array with the lowest value.
+
+The largest integer should be the integer from the array with the highest value.
+
+The minimumAbsent is the smallest number between the largest and the smallest number that is not in the array.*/
+
+function minMinMax(array) {
+  let answer = [0,0,0]
+  answer[0]=array.sort((a,b)=>a-b)[0]
+  answer[2]=array.sort((a,b)=>a-b)[array.length-1]
+  for (i=answer[0];i<answer[2];i++){
+    if (array.includes(i)!==true){
+      answer[1]=i
+      return answer
+    }
+    
+  }
+  return answer
+}
