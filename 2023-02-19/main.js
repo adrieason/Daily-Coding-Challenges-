b00fc8f9b614e67eac184db7cfac465359fa3b59
Adrianna Eason <adrianname@icloud.com>
2023-02-18 09:12:50 -0600

    CC for 2023-02-19

--- 2023-02-19/main.js
+++ 2023-02-19/main.js
@@ -1,7 +1,25 @@
-/* Write a functionn which takes in a circle object and calculates the area of that circle */
+/*The function takes in 2 inputs x and y, and should return x to the power of y
 
+Simple, right? But you're NOT allowed to use Math.pow();
 
+Obs: x and y will be naturals, so DON'T take fractions into consideration;
 
-function circleArea(circle){
-  return circle.radius*circle.radius*Math.PI
+Note : zero to the power of zero equals one in this kata
+
+Great coding <3
+
+*/
+
+function power(x,y){
+  if(x===0 && y===0){
+    return 1
+  }
+  if (y===0){
+    return 1
+  }
+  let answer = x
+  for (i=1;i<y;i++){
+    answer = answer * x
+  }
+  return answer
 }
